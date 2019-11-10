9c9b735ce38abcc8f573e4102e9b8e5c952c715f
atmaranto <35581227+atmaranto@users.noreply.github.com>
2019-11-10 06:42:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require("express"),
 	fs = require("fs"),
-	bodyParser = require("body-parser"),
 	parse = require("csv-parse"),
-	http = require("http");
+	http = require("http"),
+	child_process = require("child_process");
 
 const LAT_COLUMN = 20,
 	  LNG_COLUMN = 21,
@@ -34,38 +34,9 @@ parser.on("readable", () => {
 
 parser.end();
 
-var findClosest = (latitude, longitude) => {
-	var closest = null;
-	var distance = 1000000;
-	
-	var dist, line, lat, lng;
-	
-	for(line in dbCache) {
-		lat = parseFloat(line[LAT_COLUMN]);
-		lng = parseFloat(line[LNG_COLUMN]);
-		
-		dist = Math.sqrt(lat*lat + lng*lng);
-		if(dist < distance) {
-			distance = dist;
-			closest = line;
-		}
-	}
-	
-	if(!closest) {
-		return null;
-	}
-	
-	return {
-		temperature: parseFloat(line[TEMP_COLUMN]), dew: parseFloat(line[DEW_COLUMN]),
-		humidity: parseFloat(line[HUMID_COLUMN]), weather: line[WEATHER_COLUMN]
-	};
-};
-
 var app = express();
 
 var server = http.createServer(app);
-
-app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
 	res.set("Content-Type", "text/html");
@@ -74,23 +45,11 @@ app.get("/", (req, res) => {
 	return res.status(200).send(fileData); //Lol
 });
 
-exts.forEach((ext) => {
-	app.get("/"+ext, (req, res) => {
-		res.set("Content-Type", "text/html");
-		res.set("Content-Disposition", "inline");
-		
-		fs.readFile(ext, (data) => {
-			return res.status(200).send(data);
-		});
+app.post("/check", (req, res) => {
+	var proc = child_process.execFile("/usr/local/bin/python3", ["hackutd2019.py", req.query.temperature, req.query.latitude, req.query.longitude]);
+	proc.stdout.on("data", (data) => {
+		res.status(200).send(data.replace(/\n/g, ""));
 	});
-});
-
-app.post("/temp", (req, res) => {
-	if(typeof req.body.latitude != "number" || typeof req.body.longitude != "number") {
-		return req.status(400).send("latitude and longitude must both be specified.");
-	}
-	
-	return res.status(200).json(findClosest(req.body.latitude, req.body.longitude));
 });
 
 server.listen(400);
