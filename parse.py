24a7786233d838125708f5ff1d73f17d2b4a6f33
atmaranto <35581227+atmaranto@users.noreply.github.com>
2019-11-09 21:34:51 -0600

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,33 +1,41 @@
-import sys, math, csv
+import sys, math, csv, os
 
 if len(sys.argv) < 3:
 	print("Usage: "+sys.argv[0]+" <lat> <lng>")
-	print("--> yields nearest TEMPERATURE,DEW,HUMIDITY,WEATHER")
+	print("--> yields nearest TEMPERATURE,DEW,HUMIDITY")
 	sys.exit(1)
 
-LAT_COLUMN = 20
-LNG_COLUMN = 21
+if not os.path.isfile("Stations.csv"):
+	print("Stations.csv not found in this directory")
+	sys.exit(1)
 
-TEMP_COLUMN = 7
-DEW_COLUMN = 8
-HUMID_COLUMN = 9
-WEATHER_COLUMN = 17
+LAT_COLUMN = 19
+LNG_COLUMN = 20
+
+TEMP_COLUMN = 6
+DEW_COLUMN = 7
+HUMID_COLUMN = 8
+WEATHER_COLUMN = 16
 
 f = open("Stations.csv", "r")
 closest = None
 distance = 100000000
 
+latitude = float(sys.argv[1])
+longitude = float(sys.argv[2])
+
 f.readline()
 
 for line in csv.reader(f.readlines()):
-	lat, lng = float(line[LAT_COLUMN]), float(line[LNG_COLUMN])
+	lat, lng = (float(line[LAT_COLUMN])-latitude), (float(line[LNG_COLUMN])-longitude)
 	dist = math.sqrt(lat*lat + lng*lng)
+	
 	if dist < distance:
 		distance = dist
 		closest = line
 
 f.close()
 
-toReturn = [TEMP_COLUMN, DEW_COLUMN, HUMID_COLUMN, WEATHER_COLUMN]
+toReturn = [TEMP_COLUMN, DEW_COLUMN, HUMID_COLUMN]
 
-print(','.join([line[item] for item in toReturn]), end="")
+print(','.join([closest[item] for item in toReturn]), end="")
